ece6d4b9c8d037191450e089df54153536ad7919
Tola Bello <tolabello4@gmail.com>
2023-09-12 08:08:17 -0700

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',  // new line
+  devServer: {                 // new line
+    static: './dist'      // new line
+  },
+  plugins: [
+    new Dotenv(),
+    new CleanWebpackPlugin(), // new line
+    new ESLintPlugin({
+      context: "compiler.context",
+      eslintPath: "eslint",
+      extensions: 'js',
+      exclude: 'node_modules',
+      fix: false,
+      formatter: 'stylish',
+      lintDirtyModulesOnly: false,
+      threads: false,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: false,
+      quiet: false,
+      outputReport: false
+    }),
+    new HtmlWebpackPlugin({
+      title: 'WEATHER API',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+  }
+]
+}
+};
